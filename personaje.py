c623a243f37287f49c570b9db2c58673cff1baaa
santiemanuel <santiemanuel@gmail.com>
2024-03-06 19:09:28 -0300

    Lógica de experiencia y ataques

--- personaje.py
+++ personaje.py
@@ -1,3 +1,5 @@
+import random
+
 class Personaje:
     def __init__(self, nombre):
         self.nombre = nombre
@@ -11,6 +13,35 @@ class Personaje:
         self.defendiendo = False
         self.experiencia = 0
         self.experiencia_requerida = 100
+
+    def atacar(self, enemigo):
+        dano = self.ataque - enemigo.defensa
+        enemigo.salud -= dano
+
+    def defenderse(self):
+        self.defendiendo = True
+        print("Te has defendido y reducirás el daño del próximo ataque.")
+
+    def curarse(self, pocion):
+        self.salud += pocion.efecto
+        if self.salud > 100:
+            self.salud = 100
+
+    def subir_exp(self, exp_ganada):
+        self.experiencia += exp_ganada
+        while self.experiencia >= self.experiencia_requerida:
+            self.subir_nivel()
+            self.experiencia -= self.experiencia_requerida
+            self.experiencia_requerida = int(self.experiencia_requerida * 1.1)
+
+    def subir_nivel(self):
+        self.nivel += 1
+        print(f"¡Felicidades! Has alcanzado el nivel {self.nivel}.")
+        self.salud += random.randint(1, 3)
+        self.ataque += random.randint(1, 3)
+        self.defensa += random.randint(1, 3)
+        print(f"Tus nuevas estadísticas son: \nSalud: {self.salud} \nAtaque: {self.ataque} \nDefensa: {self.defensa}")
+
 
     def __str__(self):
         return f'{self.nombre} (Nivel {self.nivel})'
